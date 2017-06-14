8847824d0652ebd0b1d9410633b61aa6c4fe82fa
Topcu <iah31topcu@gso-koeln.de>
2017-06-14 12:55:28 +0200

    Hinzufügen,Entfernen

---
+++ BibliothekVerwaltung/BibliothekVerwaltung/Bibliothek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliothekVerwaltung
+{
+    public class Bibliothek
+    {
+        public List<string> Liste = new List<string>();
+
+        public void NameHinzufuegen(string InhaltHinzufuegen)
+        {
+            Liste.Add(InhaltHinzufuegen);
+        }
+
+        public void NameEntfernen(string InhaltEntfernen)
+        {
+            if (InhaltEntfernen == null || Liste.Contains(InhaltEntfernen))
+            {
+                throw new ArgumentOutOfRangeException("Das Spiel " + InhaltEntfernen + " kann nicht gelöscht werden!");     
+            }
+            else
+            {
+                Liste.Remove(InhaltEntfernen);
+            }
+
+            
+        }
+
+    }
+}
